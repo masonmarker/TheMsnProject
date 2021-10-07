c847a72dbd940c90ee56e4d98e1b5a1aee60433e
masonmarker <markermd@dukes.jmu.edu>
2021-10-07 11:52:23 -0400

    Update

--- Classes/MsnStructures/MsnBinaryNode.java
+++ Classes/MsnStructures/MsnBinaryNode.java
@@ -1,3 +1,4 @@
+package MsnStructures;
 
 /**
  * Node for binary tree development.
--- Testing/Main.java
+++ Testing/Main.java
@@ -1,8 +1,33 @@
+import MsnStructures.MsnGraph;
+import MsnStructures.MsnGraph.Edge;
+import MsnStructures.MsnGraph.Vertex;
+
 public class Main {
 
   public static void main(String[] args) {
 
-    System.out.println((int) 'a');
+    MsnGraph graph = new MsnGraph();
 
+
+    Vertex a = new Vertex("a");
+    Vertex b = new Vertex("b");
+    Vertex c = new Vertex("c");
+    Vertex d = new Vertex("d");
+    Vertex e = new Vertex("e");
+    Vertex f = new Vertex("f");
+    Vertex g = new Vertex("g");
+    Vertex h = new Vertex("h");
+
+
+    Edge e1 = new Edge(a, b);
+    Edge e2 = new Edge(b, c);
+    Edge e3 = new Edge(c, a);
+ 
+ 
+    graph.visualize(a, b, c, a, g);
+     
+    System.out.println(graph.isComplete());
+    
+        
   }
 }
