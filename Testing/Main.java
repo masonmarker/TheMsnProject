32ecc285522086d4246114bb40af2a946c709c8d
masonmarker <markermd@dukes.jmu.edu>
2021-11-29 16:32:52 -0500

    MsnStream Update 1

--- Testing/Main.java
+++ Testing/Main.java
@@ -6,10 +6,45 @@ public class Main {
 
 
     new MsnStream<Integer>()._importRange(0, 5)._addDuplicate()._print()._isolateDuplicates()
-        ._print()._forEach(integer -> {
-          integer += 2;
-          integer -= 2;
-        })._add(10)._add(10)._isolateDuplicates()._print();
+        ._print()._map(i -> i += 2)._add(0)._addDuplicate()._print()._forEach(i -> {
+          System.out.print(" el=");
+          System.out.print(i);
+        })._print("\n").halfSize()._add(1)._sorted()._map(i -> i += 1)._print().halfSize()._addMax()
+        ._addMin()._print()._withoutDuplicates()._sorted()._print()._importRange(0, 5)
+        ._map(i -> i -= 2)._withoutDuplicates()._sorted()._print()._map(i -> i = 0)._forEach(i -> {
+          System.out.print("fuck");
+        })._print("\n")._print()._isolateDuplicates()._empty()._importRange(5, 10)._map(i -> i += i)
+        ._print()._addMax()._insert(0, 1000)._insert(0, 1000)._print()._importRange(0, 5)
+        ._addDuplicate()._print()._isolateDuplicates()._print()._map(i -> i += 2)._add(0)
+        ._addDuplicate()._print()._forEach(i -> {
+          System.out.print(" el=");
+          System.out.print(i);
+        })._print("\n").halfSize()._add(1)._sorted()._map(i -> i += 1)._print().halfSize()._addMax()
+        ._addMin()._print()._withoutDuplicates()._sorted()._print()._importRange(0, 5)
+        ._map(i -> i -= 2)._withoutDuplicates()._sorted()._print()._map(i -> i = 0)._forEach(i -> {
+          System.out.print("fuck");
+        })._print("\n")._print()._isolateDuplicates()._empty()._importRange(5, 10)._map(i -> i += i)
+        ._print()._addMax()._insert(0, 1000)._insert(0, 1000)._print()._importRange(0, 5)
+        ._addDuplicate()._print()._isolateDuplicates()._print()._map(i -> i += 2)._add(0)
+        ._addDuplicate()._print()._forEach(i -> {
+          System.out.print(" el=");
+          System.out.print(i);
+        })._print("\n").halfSize()._add(1)._sorted()._map(i -> i += 1)._print().halfSize()._addMax()
+        ._addMin()._print()._withoutDuplicates()._sorted()._print()._importRange(0, 5)
+        ._map(i -> i -= 2)._withoutDuplicates()._sorted()._print()._map(i -> i = 0)._forEach(i -> {
+          System.out.print("fuck");
+        })._print("\n")._print()._isolateDuplicates()._empty()._importRange(5, 10)._map(i -> i += i)
+        ._print()._addMax()._insert(0, 1000)._insert(0, 1000)._print()._importRange(0, 5)
+        ._addDuplicate()._print()._isolateDuplicates()._print()._map(i -> i += 2)._add(0)
+        ._addDuplicate()._print()._forEach(i -> {
+          System.out.print(" el=");
+          System.out.print(i);
+        })._print("\n").halfSize()._add(1)._sorted()._map(i -> i += 1)._print().halfSize()._addMax()
+        ._addMin()._print()._withoutDuplicates()._sorted()._print()._importRange(0, 5)
+        ._map(i -> i -= 2)._withoutDuplicates()._sorted()._print()._map(i -> i = 0)._forEach(i -> {
+          System.out.print("fuck");
+        })._print("\n")._print()._isolateDuplicates()._empty()._importRange(5, 10)._map(i -> i += i)
+        ._print()._addMax()._insert(0, 1000)._insert(0, 1000)._print();
 
   }
 }
