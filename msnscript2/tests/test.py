3845c0dce6edbf2209e34701ae110b8ded482964
masonmarker <mason1marker@gmail.com>
2022-11-15 21:29:21 -0500

    MSNscript2 Update

--- msnscript2/tests/test.py
+++ msnscript2/tests/test.py
@@ -1,24 +1,32 @@
+def bubbleSort(arr):
+    n = len(arr)
+    # optimize code, so if the array is already sorted, it doesn't need
+    # to go through the entire process
+    swapped = False
+    # Traverse through all array elements
+    for i in range(n-1):
+        # range(n) also work but outer loop will
+        # repeat one time more than needed.
+        # Last i elements are already in place
+        for j in range(0, n-i-1):
+            print(arr)
+            # traverse the array from 0 to n-i-1
+            # Swap if the element found is greater
+            # than the next element
+            if arr[j] > arr[j + 1]:
+                swapped = True
+                arr[j], arr[j + 1] = arr[j + 1], arr[j]
+         
+        if not swapped:
+            # if we haven't needed to make a single swap, we
+            # can just exit the main loop.
+            return
 
 
-map1 = {'h': 1, 'e': 1, 'l': 2, 'o': 1}
-map2 = {'g': 1, 'o': 2, 'f': 1, 'y': 1}
+array = [6, 5, 4, 3, 2, 1]
 
-# hello
-# goofy
+bubbleSort(array)
 
-changes = 0
-
-
-for char, freq in map1.items():
-    if char not in map2.keys():
-        changes += 1
-    else:
-        changes += abs(map2[char] - map1[char])
-
-
-print(changes)
-
-
-
+print(array)
 
 
