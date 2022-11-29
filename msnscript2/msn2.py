1995d9a8a7d2a92561983ab64519d3aa886abaca
masonmarker <mason1marker@gmail.com>
2022-11-29 10:00:15 -0500

    MSNScript update

--- msnscript2/msn2.py
+++ msnscript2/msn2.py
@@ -7,22 +7,26 @@ import msnint2
 # cmd argument support
 import sys
 
-# filename is the first argument
-filename = sys.argv[1]
+# filenames to run
+if len(sys.argv) <= 1:
+    print('[-] at least one .msn2 file needs to be specified')
+    exit(1)
 
-extension = filename[filename.rindex('.'):]
+for i in range(1, len(sys.argv)):
+    filename = sys.argv[i]
+    extension = filename[filename.rindex('.'):]
 
-# verify file type
-if extension != '.msn2':
-    print("Error: file type not supported")
-    exit()
+    # verify file type
+    if extension != '.msn2':
+        print("Error: file type not supported")
+        exit()
 
 
-f = open(filename, "r")
-script = f.read()
+    f = open(filename, "r")
+    script = f.read()
 
-interpreter = msnint2.Interpreter()
-interpreter.execute(script)
+    interpreter = msnint2.Interpreter()
+    interpreter.execute(script)
 
-if interpreter.out != '':
-    print(interpreter.out)
+    if interpreter.out != '':
+        print(interpreter.out)
