fc7a0234d3811b58054514c7c2ab4ff2de3d20c4
masonmarker <markermd@dukes.jmu.edu>
2023-01-18 19:11:56 -0500

    MSN2 update

--- msnscript2/tests/test.py
+++ msnscript2/tests/test.py
@@ -1,1 +1,23 @@
 
+
+print(int(input("What is the number: ")) / 300)
+
+
+# number7 = 7
+
+# added = 1+number7+1
+
+
+# print(added)
+
+
+
+# number = 2
+# string = "2"
+
+
+# print(2 + int("2"))
+
+
+
+
