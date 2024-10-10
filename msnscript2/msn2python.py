bdd44318a5cc089fd78a359400c69ef22f0cdc08
masonmarker <markermd@dukes.jmu.edu>
2024-10-09 22:59:50 -0400

    Updated msn2python runner to handle files not found

--- msnscript2/msn2python.py
+++ msnscript2/msn2python.py
@@ -5,6 +5,7 @@
 #
 # author : Mason Marker
 # date : 6/2/2023
+# updated : 10/09/2024
 
 # provides a .msn2 file with input values, and receives the output value
 #
@@ -13,41 +14,49 @@
 # see the .msn2 file in this directory for how input
 # values are read and output
 
+import os
+
 # prepare msn2 interpreter
-import msnint2
+from msnint2 import Interpreter, Var
 
-# cmd argument support  
-import sys
 
-# executes the script
-#
-# filename: the path to the .msn2 file to be executed
-# inputs: a list of values to be passed to the script
 def run(filename, inputs=None):
-
+    # if no inputs are provided, set to empty list
+    if not os.path.exists(filename):
+        # print a unique not found "error" and quit
+        i = Interpreter()
+        i.styled_print(
+            [
+                {"text": "[", "fore": "black"},
+                {"text": "MSN2", "fore": "yellow"},
+                {"text": "]", "fore": "black"},
+                {"text": f" File not found: {filename}", "fore": "red"},
+            ]
+        )
+        exit()
     # opens the file for reading
     f = open(filename, "r", encoding="utf-8")
     script = f.read()
-    
+
     # create an Interpreter to run the script
-    interpreter = msnint2.Interpreter()
-    
+    interpreter = Interpreter()
+
     # reserved keyword for input
-    _n = '_msn2_reserved_in__'
-    
+    _n = "_msn2_reserved_in__"
+
     # pass the inputs to the script
-    interpreter.vars[_n] = msnint2.Var(_n, inputs)
-    
+    interpreter.vars[_n] = Var(_n, inputs)
+
     # execute the script
     interpreter.execute(script)
 
     # prints the prnt() out if specified
-    if interpreter.out != '':
+    if interpreter.out != "":
         print(interpreter.out)
-        
+
     # returns the exported value from the script
-    
-    _n_out = '_msn2_reserved_out__'
-    
+
+    _n_out = "_msn2_reserved_out__"
+
     if _n_out in interpreter.vars:
         return interpreter.vars[_n_out].value
